61aebf0dde88e765441a97b27c0846ec19a5d7b2
prat778 <pratham016914934@gmail.com>
2024-07-25 21:00:58 +0545

    added class based views

--- restTutorial/myapp/api_file/serializers.py
+++ restTutorial/myapp/api_file/serializers.py
@@ -1,11 +1,31 @@
 from rest_framework import serializers
 from .. models import CarList
-class SerializeData(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    desc = serializers.CharField()
-    active=serializers.BooleanField(read_only=True)
+
+# def alphanum(value):
+#     if not str(value).isalnum():
+#         raise serializers.ValidationError("License no should be alphanumeric")
+#     return value
+
+class SerializeData(serializers.ModelSerializer):
+    # id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField()
+    # desc = serializers.CharField()
+    # active = serializers.BooleanField(read_only=True)
+    # price = serializers.DecimalField(max_digits=9, decimal_places=2)
+    # license_no = serializers.CharField(validators=[alphanum])
+
+    #custom fields
+    discounted_price = serializers.SerializerMethodField()
+    class Meta:
+        model = CarList
+        fields = "__all__"
+        read_only_fields=['id','active']
+        # fields= ['name','desc','price']
     
+    def get_discounted_price(self,object):
+        d_price= object.price - 5000 
+        return d_price
+
     def create(self, validated_data):
         return CarList.objects.create(**validated_data)
     
@@ -13,5 +33,17 @@ class SerializeData(serializers.Serializer):
         instance.name=validated_data.get('name',instance.name)
         instance.desc=validated_data.get('desc',instance.desc)
         instance.active=validated_data.get('active',instance.active)
+        instance.price=validated_data.get('price',instance.price)
+        instance.license_no = validated_data.get('license_no',instance.license_no)
         instance.save()
         return instance
+    
+    def validate_price(self,value):
+        if(value<=20000):
+            raise serializers.ValidationError("Price must be greater than 20000")
+        return value
+
+    def validate(self,data):
+        if data['name']==data['desc']:
+            raise serializers.ValidationError('Name and description must be different')
+        return data
--- restTutorial/myapp/models.py
+++ restTutorial/myapp/models.py
@@ -1,11 +1,17 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+def alphanum(value):
+        if not str(value).isalnum():
+            raise ValidationError("License no should be alphanumeric")
+        return value
 class CarList(models.Model):
+    
     name = models.CharField(max_length=50)
     desc=models.CharField(max_length=200)
     active = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=9,decimal_places=2,blank=True,null=True)
-    license_no=models.CharField(max_length=100, blank=True,null=True)
+    license_no=models.CharField(max_length=100, blank=True,null=True, validators=[alphanum])
     def __str__(self):
         return self.name
